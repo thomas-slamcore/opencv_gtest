5f9b6bac121d824690d40da842e7c3591c243f5c
Thomas Bastiani <thomas@slamcore.com>
2020-09-30 15:59:19 +0100

    Add some boilerplate

--- test.cpp
+++ test.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
+#include <opencv2/core.hpp>
 
-TEST(Dummy, dummy)
+void sobelFilter(const cv::Mat& src, cv::Mat& dst)
 {
-  EXPECT_TRUE(true);
+  static const float kernelData[] =
+  {
+    +1, 0, -1,
+    +2, 0, -2,
+    +1, 0, -1,
+  };
+  static const cv::Mat kernel(3, 3, CV_32FC1, const_cast<float*>(kernelData));
+
+  // TODO implement this
+}
+
+TEST(Sobel, dummy)
+{
+  cv::Mat image(1024, 1024, CV_8UC1);
+  cv::Mat edges;
+
+  sobelFilter(image, edges);
+  EXPECT_FALSE(edges.empty());
 }
